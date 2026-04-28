4781e8d07f7f7d53047534dc5a1711f14943e4b5
RafaelQueirozViana <rafaelviana.contact@gmail.com>
2026-04-28 11:00:53 -0300

    criacao cadastro de usuario

--- demo/src/main/java/com/chamados/demo/model/Usuario.java
+++ demo/src/main/java/com/chamados/demo/model/Usuario.java
@@ -1,6 +1,8 @@
 package com.chamados.demo.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 @Entity
 public class Usuario {
@@ -9,8 +11,22 @@ public class Usuario {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     private String nome;
+
+    @Email
     private String email;
 
 
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getNome() { return nome; }
+
+    public void setNome(String nome) { this.nome = nome; }
+
+    public String getEmail() { return email; }
+
+    public void setEmail(String email) { this.email = email; }
 }
--- demo/src/main/java/com/chamados/demo/repositorio/UsuarioRepository.java
+++ demo/src/main/java/com/chamados/demo/repositorio/UsuarioRepository.java
@@ -4,4 +4,5 @@ import com.chamados.demo.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
 }
