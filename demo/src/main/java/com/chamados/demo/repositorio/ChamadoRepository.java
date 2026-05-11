23d2326f4aecb7b0e6984ba90a55e947b57eecab
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-05-11 14:22:49 -0300

    mvc de chamado

--- demo/src/main/java/com/chamados/demo/repositorio/ChamadoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chamados.demo.repositorio;
-
-import com.chamados.demo.model.Chamado;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChamadoRepository extends JpaRepository<Chamado, Long> {
-}
