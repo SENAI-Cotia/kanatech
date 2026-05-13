e0194ad4d8579d9286ff0e36a31ea83cc87b2f87
ThiagoRelias <thiagorelias04052007@gmail.com>
2026-05-13 15:48:52 -0300

    Criação de sistema de login

--- src/main/java/com/chamados/demo/controller/ChamadoController.java
+++ src/main/java/com/chamados/demo/controller/ChamadoController.java
@@ -5,6 +5,7 @@ import com.chamados.demo.model.StatusChamado;
 import com.chamados.demo.service.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +19,11 @@ public class ChamadoController {
     @Autowired
     private ChamadoService chamadoService;
 
-
     @GetMapping("/")
     public String formulario(Model model) {
         model.addAttribute("chamado", new Chamado());
         return "formulario";
     }
-
     @PostMapping("/chamado/novo")
     public String novoChamado(@ModelAttribute Chamado chamado, Model model) {
         chamadoService.salvar(chamado);
@@ -32,9 +31,7 @@ public class ChamadoController {
         model.addAttribute("chamado", new Chamado());
         return "formulario";
     }
-
-
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/ti/chamados")
     public String listarChamados(Model model) {
         List<Chamado> chamados = chamadoService.listarTodos();
@@ -42,27 +39,31 @@ public class ChamadoController {
         model.addAttribute("statusList", StatusChamado.values());
         return "chamados";
     }
-
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/ti/chamado/{id}/status")
     @ResponseBody
     public ResponseEntity<?> atualizarStatus(
             @PathVariable Long id,
             @RequestBody Map<String, String> body) {
+
         try {
             StatusChamado novoStatus = StatusChamado.valueOf(body.get("status"));
             chamadoService.atualizarStatus(id, novoStatus);
-            return ResponseEntity.ok(Map.of("sucesso", true));
+
+            return ResponseEntity.ok(Map.of(
+                    "sucesso", true
+            ));
         } catch (Exception e) {
-            return ResponseEntity.badRequest().body(Map.of("erro", e.getMessage()));
+            return ResponseEntity.badRequest().body(Map.of(
+                    "erro", e.getMessage()
+            ));
         }
     }
-
-
-
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/ti/chamado/{id}")
     @ResponseBody
     public ResponseEntity<?> buscarChamado(@PathVariable Long id) {
+
         return chamadoService.buscarPorId(id)
                 .map(c -> ResponseEntity.ok(Map.of(
                         "id", c.getId(),
